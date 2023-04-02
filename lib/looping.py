3d1475b00dca0e515d1f74591480e615e133eb1a
zinakalu <zina.kalu@gmail.com>
2023-04-02 10:32:47 -0400

    looping

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
     pass
 
 def square_integers(int_list):
-    # code goes here!
+    return [i*i for i in int_list]
     pass
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else: 
+            print(i)
+
+# fizzbuzz()
+
